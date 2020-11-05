fd6beeb69051ab53d98648d4333acd3cfde8350c
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-11-05 16:58:43 +0100

    Add framework's skeleton

---
+++ Framework/Porcelain/Porcelain.h
@@ -0,0 +1,18 @@
+//
+//  Porcelain.h
+//  Porcelain
+//
+//  Created by Mateusz Stompór on 05/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Porcelain.
+FOUNDATION_EXPORT double PorcelainVersionNumber;
+
+//! Project version string for Porcelain.
+FOUNDATION_EXPORT const unsigned char PorcelainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Porcelain/PublicHeader.h>
+
+
