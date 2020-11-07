9fa5dfc7e7ed2b20059c04751440994cb6f51f38
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-11-07 19:02:37 +0100

    Make camera coordinate system mutable

--- Framework/Porcelain/Scenery/Camera.swift
+++ Framework/Porcelain/Scenery/Camera.swift
@@ -11,7 +11,7 @@ public struct Camera {
     // MARK: - Properties
     internal let projectionMatrix: matrix_float4x4
     // Radians
-    public let coordinateSpace: CoordinateSpace
+    public var coordinateSpace: CoordinateSpace
     // MARK: - Initialization
     public init(nearPlane: Float, farPlane: Float, fovRadians: Float, aspectRation: Float, coordinateSpace: CoordinateSpace) {
         self.projectionMatrix = matrix_float4x4.perspectiveProjectionRightHand(fovyRadians: fovRadians,
