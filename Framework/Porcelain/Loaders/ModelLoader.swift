b2f7964f9144585bf43014261a3d500658640ba2
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-11-07 18:56:26 +0100

    Make model loading more readable

--- Framework/Porcelain/Loaders/ModelLoader.swift
+++ Framework/Porcelain/Loaders/ModelLoader.swift
@@ -9,8 +9,8 @@ import MetalKit
 
 
 struct Vertex {
-    let position: vector_float3
-    let normal: vector_float3
+    let position: simd_float3
+    let normal: simd_float3
 }
 
 public struct ModelLoader {
@@ -22,12 +22,16 @@ public struct ModelLoader {
     }
     public func loadAsset(name: String, extension: String) -> MDLAsset? {
         let vertexDescriptor = MDLVertexDescriptor()
-        let vertexLayout = MDLVertexBufferLayout()
-        vertexLayout.stride = MemoryLayout<Vertex>.size
-        vertexDescriptor.layouts = [vertexLayout]
+        vertexDescriptor.layouts = [MDLVertexBufferLayout(stride: MemoryLayout<Vertex>.stride)]
         vertexDescriptor.attributes = [
-            MDLVertexAttribute(name: MDLVertexAttributePosition, format: .float3, offset: 0, bufferIndex: 0),
-            MDLVertexAttribute(name: MDLVertexAttributeNormal, format: .float3, offset: MemoryLayout<Vertex>.size/2, bufferIndex: 0)
+            MDLVertexAttribute(name: MDLVertexAttributePosition,
+                               format: .float3,
+                               offset: MemoryLayout<Vertex>.offset(of: \Vertex.position)!,
+                               bufferIndex: 0),
+            MDLVertexAttribute(name: MDLVertexAttributeNormal,
+                               format: .float3,
+                               offset: MemoryLayout<Vertex>.offset(of: \Vertex.normal)!,
+                               bufferIndex: 0)
         ]
         guard let url = Bundle.main.url(forResource: name, withExtension: `extension`) else {
             return nil
