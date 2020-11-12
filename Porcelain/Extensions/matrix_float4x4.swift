4457c1b3b83958b6651e1871e0c14df3ee5c764e
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-11-13 00:13:42 +0100

    Fix conversion of vector to translation matrix

--- Porcelain/Extensions/matrix_float4x4.swift
+++ Porcelain/Extensions/matrix_float4x4.swift
@@ -18,9 +18,9 @@ extension matrix_float4x4 {
                                           simd_float4(0,  0, -1, 0 )])
     }
     static func translation(vector: simd_float3) -> matrix_float4x4 {
-        return matrix_float4x4.init(rows:[simd_float4(0,  0, 0, 0),
-                                          simd_float4(0,  0, 0, 0),
-                                          simd_float4(0,  0, 0, 0),
-                                          simd_float4(vector, 0)])
+        return matrix_float4x4.init(rows:[simd_float4(1,  0, 0, 0),
+                                          simd_float4(0,  1, 0, 0),
+                                          simd_float4(0,  0, 1, 0),
+                                          simd_float4(vector, 1)]).transpose
     }
 }
