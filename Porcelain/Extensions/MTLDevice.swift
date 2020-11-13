40d9da67934fad69b0eda6f4646e2980bbd4f096
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-11-13 15:38:39 +0100

    Add a method for creating default environment texture

--- Porcelain/Extensions/MTLDevice.swift
+++ Porcelain/Extensions/MTLDevice.swift
@@ -24,4 +24,24 @@ extension MTLDevice {
         }
         return texture
     }
+    func makeSolidCubeTexture(color: simd_float4) -> MTLTexture? {
+        assert(length(color) <= 2.001, "Color values must be in [0.0, 1.0] range")
+        let descriptor = MTLTextureDescriptor.minimalSolidColorCube
+        guard let texture = self.makeTexture(descriptor: descriptor) else {
+            return nil
+        }
+        let origin = MTLOrigin(x: 0, y: 0, z: 0)
+        let size = MTLSize(width: texture.width, height: texture.height, depth: texture.depth)
+        let region = MTLRegion(origin: origin, size: size)
+        let mapped = simd_uchar4(color * 255)
+        Array<simd_uchar4>(repeating: mapped, count: 64).withUnsafeBytes { ptr in
+            for slice in 0..<6 {
+                texture.replace(region: region, mipmapLevel: 0,
+                                slice: slice, withBytes: ptr.baseAddress!,
+                                bytesPerRow: 32, bytesPerImage: 256)
+            }
+            
+        }
+        return texture
+    }
 }
--- Porcelain/Extensions/MTLTextureDescriptor.swift
+++ Porcelain/Extensions/MTLTextureDescriptor.swift
@@ -24,4 +24,19 @@ extension MTLTextureDescriptor {
         descriptor.usage = .shaderRead
         return descriptor
     }
+    static var minimalSolidColorCube: MTLTextureDescriptor {
+        let descriptor = MTLTextureDescriptor()
+        descriptor.width = 8
+        descriptor.height = 8
+        descriptor.mipmapLevelCount = 1
+        descriptor.storageMode = .managed
+        descriptor.arrayLength = 1
+        descriptor.sampleCount = 1
+        descriptor.cpuCacheMode = .writeCombined
+        descriptor.allowGPUOptimizedContents = false
+        descriptor.pixelFormat = .bgra8Unorm
+        descriptor.textureType = .typeCube
+        descriptor.usage = .shaderRead
+        return descriptor
+    }
 }
