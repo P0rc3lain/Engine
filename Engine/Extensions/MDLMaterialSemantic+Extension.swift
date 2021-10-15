ab26f518b747b031021cba0f0a6d95ba34229a5b
Mateusz Stompór <mateusz.stompor@gmail.com>
2021-10-15 18:30:38 +0200

    Get rid of duplicated files

--- Engine/Extensions/MDLMaterialSemantic+Extension.swift
+++ Engine/Extensions/MDLMaterialSemantic+Extension.swift
@@ -8,6 +8,24 @@
 import ModelIO
 
 extension MDLMaterialSemantic {
+    var label: String {
+        switch self {
+        case .baseColor:
+            return "albedo"
+        case .tangentSpaceNormal:
+            return "normal"
+        case .metallic:
+            return "metallic"
+        case .roughness:
+            return "roughness"
+        case .specularExponent:
+            return "specularExponent"
+        case .bump:
+            return "bump"
+        default:
+            fatalError("Not implemented")
+        }
+    }
     var defaultColor: simd_float4 {
         switch self {
         case .baseColor:
