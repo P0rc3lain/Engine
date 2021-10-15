c58f84c4a473c3008dfd2fc2eb6a5483168fc3bc
Mateusz Stompór <mateusz.stompor@gmail.com>
2021-10-15 18:20:08 +0200

    Rearrange project

--- Engine/Extensions/Texture+Extension.swift
+++ Engine/Extensions/Texture+Extension.swift
@@ -6,6 +6,7 @@
 //
 
 import simd
+import MetalKit
 
 extension Texture {
     static func solid2D(color: simd_float4) -> Texture {
@@ -16,5 +17,32 @@ extension Texture {
                        channelEncoding: .float32,
                        isCube: false,
                        mipLevelCount: 0)
+    }
+    func upload(device: MTLDevice) -> MTLTexture? {
+        let loader = MTKTextureLoader.init(device: device)
+        return try! loader.newTexture(texture: self.texture, options: nil)
+        
+        
+        let descriptor = MTLTextureDescriptor()
+        descriptor.textureType = .type2D
+        descriptor.width = Int(dimensions.x)
+        descriptor.height = Int(dimensions.y)
+        descriptor.storageMode = .managed
+        descriptor.pixelFormat = .rgba16Float
+        descriptor.usage = .shaderRead
+        guard let texture = device.makeTexture(descriptor: descriptor) else {
+            return nil
+        }
+        let origin = MTLOrigin(x: 0, y: 0, z: 0)
+        let size = MTLSize(width: Int(dimensions.x), height: Int(dimensions.y),
+                           depth: 1)
+        let region = MTLRegion(origin: origin, size: size)
+        data.withUnsafeBytes { ptr in
+            texture.replace(region: region,
+                            mipmapLevel: mipLevelCount,
+                            withBytes: ptr.baseAddress!,
+                            bytesPerRow: rowStride)
+        }
+        return texture
     }
 }
