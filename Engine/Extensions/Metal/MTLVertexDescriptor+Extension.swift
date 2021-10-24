718e24b0ff86537471ef7a4e0dda82ada1c29d67
Mateusz Stompór <mateusz.stompor@gmail.com>
2021-10-24 22:43:38 +0200

    Merge instantiation and extensions

--- Engine/Extensions/Metal/MTLVertexDescriptor+Extension.swift
+++ Engine/Extensions/Metal/MTLVertexDescriptor+Extension.swift
@@ -12,4 +12,12 @@ extension MTLVertexDescriptor {
     public static var porcelain: MTLVertexDescriptor {
         MTKMetalVertexDescriptorFromModelIO(MDLVertexDescriptor.porcelain)!
     }
+    static var environmentRenderer: MTLVertexDescriptor {
+        let descriptor = MTLVertexDescriptor()
+        descriptor.layouts[0] = MTLVertexBufferLayoutDescriptor.environmentRenderer
+        descriptor.attributes[0].format = .float4
+        descriptor.attributes[0].offset = 0
+        descriptor.attributes[0].bufferIndex = 0
+        return descriptor
+    }
 }
