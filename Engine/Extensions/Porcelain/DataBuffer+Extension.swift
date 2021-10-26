f0d32e14d1d5ed6718ea9aacafdc1aa5f734ad33
Mateusz Stompór <mateusz.stompor@gmail.com>
2021-10-26 20:13:46 +0200

    Simplify buffer operations

--- Engine/Extensions/Porcelain/DataBuffer+Extension.swift
+++ Engine/Extensions/Porcelain/DataBuffer+Extension.swift
@@ -6,16 +6,8 @@ import Metal
 
 extension RamDataBuffer {
     func upload(device: MTLDevice) -> GPUDataBuffer? {
-        guard let deviceBuffer = device.makeBuffer(length: length,
-                                                options: .storageModeShared) else {
+        guard let deviceBuffer = device.makeBuffer(data: buffer) else {
             return nil
-        }
-        let deviceBufferPointer = deviceBuffer.contents()
-        buffer.withUnsafeBytes { (pointer: UnsafeRawBufferPointer) in
-            guard let baseAddress = pointer.baseAddress else {
-                fatalError("Cannot retrieve base address of the pointer")
-            }
-            deviceBufferPointer.copyMemory(from: baseAddress, byteCount: length)
         }
         return GPUDataBuffer(buffer: deviceBuffer, length: length, offset: offset)
     }
