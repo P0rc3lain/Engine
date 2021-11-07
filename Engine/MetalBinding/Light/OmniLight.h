f70ed98a59568f873b42abab34740850aabacaed
Mateusz Stompór <mateusz.stompor@gmail.com>
2021-11-07 13:33:17 +0100

    Remove unused code

--- Engine/MetalBinding/Light/OmniLight.h
+++ Engine/MetalBinding/Light/OmniLight.h
@@ -5,12 +5,6 @@
 #ifndef OMNILIGHT_H
 #define OMNILIGHT_H
 
-struct OmniLightUniforms {
-    simd_float4x4 projectionMatrix;
-    simd_float4x4 viewMatrix;
-    simd_float4x4 viewMatrixInverse;
-};
-
 struct OmniLight {
     simd_float3 color;
     float intensity;
