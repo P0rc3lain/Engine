773d7f13e2c45e6ce0c152c43f50b082ab091d28
Mateusz Stompór <mateusz.stompor@gmail.com>
2021-11-30 22:30:53 +0100

    Implement pcf shadows for directional lights

---
+++ Engine/Shaders/Common/Shadow.h
@@ -0,0 +1,18 @@
+//
+//  Copyright © 2021 Mateusz Stompór. All rights reserved.
+//
+
+#ifndef SHADOW_H
+#define SHADOW_H
+
+#include <simd/simd.h>
+#include <metal_stdlib>
+
+float pcfDepth(metal::depth2d_array<float> shadowMaps,
+               uint layer,
+               float2 sampleCoordinate,
+               int2 samples,
+               float countedDepth,
+               float bias);
+
+#endif /* SHADOW_H */
