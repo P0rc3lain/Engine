0ae87cf8596e1612dc109af191b09b721bb2a7d4
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-05-27 15:04:20 +0200

    Fix bound calculations

---
+++ EngineTests/Scene/Bounds/Bound/PNIBoundEstimator+Tests.swift
@@ -0,0 +1,56 @@
+//
+//  Copyright © 2022 Mateusz Stompór. All rights reserved.
+//
+
+@testable import Engine
+import simd
+import XCTest
+import MetalBinding
+
+class PNIBoundEstimatorTests: XCTestCase {
+    func testSingleVertexArray() {
+        let vertex = Vertex(position: .zero,
+                            normal: [0, 1, 0],
+                            tangent: [1, 0, 0],
+                            textureUV: .zero)
+        let bound = PNIBoundEstimator().bound(vertexBuffer: [vertex])
+        XCTAssertEqual(bound.min, .zero)
+        XCTAssertEqual(bound.max, .zero)
+    }
+    func testTwoVertexArray() {
+        let vertices = [
+            Vertex(position: .zero,
+                   normal: [0, 1, 0],
+                   tangent: [1, 0, 0],
+                   textureUV: .zero),
+            Vertex(position: .one,
+                   normal: [0, 1, 0],
+                   tangent: [1, 0, 0],
+                   textureUV: .zero)]
+        let bound = PNIBoundEstimator().bound(vertexBuffer: vertices)
+        XCTAssertEqual(bound.min, .zero)
+        XCTAssertEqual(bound.max, .one)
+    }
+    func testMultipleVertices() {
+        let vertices = [
+            Vertex(position: [-1, -5, -3],
+                   normal: [0, 1, 0],
+                   tangent: [1, 0, 0],
+                   textureUV: .zero),
+            Vertex(position: [-4, -2, -6],
+                   normal: [0, 1, 0],
+                   tangent: [1, 0, 0],
+                   textureUV: .zero),
+            Vertex(position: [4, 2, 6],
+                   normal: [0, 1, 0],
+                   tangent: [1, 0, 0],
+                   textureUV: .zero),
+            Vertex(position: [1, 5, 3],
+                   normal: [0, 1, 0],
+                   tangent: [1, 0, 0],
+                   textureUV: .zero)]
+        let bound = PNIBoundEstimator().bound(vertexBuffer: vertices)
+        XCTAssertEqual(bound.min, [-4, -5, -6])
+        XCTAssertEqual(bound.max, [4, 5, 6])
+    }
+}
