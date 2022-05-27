275c4da0d7a9930d45cac6b2e12a3090210c8fd7
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-05-27 15:18:56 +0200

    Add a method for getting a random, perpendicular vector

--- Engine/Extensions/simd/simd_float3+Extension.swift
+++ Engine/Extensions/simd/simd_float3+Extension.swift
@@ -9,9 +9,16 @@ extension simd_float3 {
         sqrtf(dot(self, self))
     }
     public static func random(componentRange: Range<Float> = 0 ..< 1) -> simd_float3 {
-        simd_float3(.random(in: componentRange), .random(in: componentRange), .random(in: componentRange))
+        simd_float3(.random(in: componentRange),
+                    .random(in: componentRange),
+                    .random(in: componentRange))
     }
     public var normalized: simd_float3 {
         self / norm
+    }
+    public func randomPerpendicular(length: Float = 1) -> simd_float3 {
+        let randomVector = simd_float3.random()
+        let dotProduct = dot(randomVector, self)
+        return (self - (self * dotProduct)).normalized * length
     }
 }
