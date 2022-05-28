b13666f853d8bcca6ccfe062c0820b7df74c2727
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-05-28 17:40:00 +0200

    Add missing file

---
+++ Engine/Extensions/Foundation/Bundle+Extension.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2022 Mateusz Stompór. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    static var porcelain: Bundle {
+        Bundle(for: PNIEngine.self)
+    }
+}
