30161da504a4bee1743bdc227330952a78067cc2
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-05-31 14:39:29 +0200

    Add initializer helping in material creation

--- Engine/Scene/Material/PNIMaterial.swift
+++ Engine/Scene/Material/PNIMaterial.swift
@@ -22,6 +22,31 @@ public struct PNIMaterial: PNMaterial {
         self.normals = normals
         self.metallic = metallic
     }
+    public init?(device: MTLDevice,
+                 albedo: simd_float4,
+                 metallic metallicValue: Float,
+                 roughness roughnessValue: Float,
+                 name: String = "") {
+        assert(metallicValue >= 0 && metallicValue <= 1, "Metallic must be in range [0, 1]")
+        assert(roughnessValue >= 0 && roughnessValue <= 1, "Roughness must be in range [0, 1]")
+        let normals = MDLTexture.solid2D(color: .defaultNormalsColor, name: "Normals")
+        let albedo = MDLTexture.solid2D(color: albedo, name: "Albedo")
+        let metallic = MDLTexture.solid2D(color: simd_float4(simd_float3(repeating: metallicValue), 1),
+                                          name: "Metallic")
+        let roughness = MDLTexture.solid2D(color: simd_float4(simd_float3(repeating:roughnessValue), 1),
+                                           name: "Roughness")
+        guard let uN = normals.upload(device: device),
+              let uM = metallic.upload(device: device),
+              let uR = roughness.upload(device: device),
+              let uA = albedo.upload(device: device) else {
+            return nil
+        }
+        self.roughness = uR
+        self.albedo = uA
+        self.normals = uN
+        self.metallic = uM
+        self.name = name
+    }
     public static func `default`(device: MTLDevice) -> PNIMaterial? {
         let normals = MDLTexture.solid2D(color: .defaultNormalsColor,
                                          name: "Default Normals")
