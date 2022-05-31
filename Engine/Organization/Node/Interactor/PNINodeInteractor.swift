78beea5341e1204a133cc4ea78aee2da984b1db5
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-05-31 13:36:59 +0200

    Make node interactor public

--- Engine/Organization/Node/Interactor/PNINodeInteractor.swift
+++ Engine/Organization/Node/Interactor/PNINodeInteractor.swift
@@ -2,17 +2,18 @@
 //  Copyright © 2021 Mateusz Stompór. All rights reserved.
 //
 
-struct PNINodeInteractor: PNNodeInteractor {
-    func forEach<T>(node: PNNode<T>, _ closure: (PNNode<T>) -> Void) {
+public struct PNINodeInteractor: PNNodeInteractor {
+    public init() { }
+    public func forEach<T>(node: PNNode<T>, _ closure: (PNNode<T>) -> Void) {
         forEach(node: node) { n, _ in closure(n) }
     }
-    func forEach<T, P>(node: PNNode<T>, passingClosure: (PNNode<T>, P?) -> P?) {
+    public func forEach<T, P>(node: PNNode<T>, passingClosure: (PNNode<T>, P?) -> P?) {
         forEach(node: node, passingClosure: passingClosure, initial: nil)
     }
     private func forEach<T, P>(node: PNNode<T>, passingClosure: (PNNode<T>, P?) -> P?, initial: P?) {
         let value = passingClosure(node, initial)
-        for child in node.children {
-            forEach(node: child, passingClosure: passingClosure, initial: value)
+        node.children.forEach {
+            forEach(node: $0, passingClosure: passingClosure, initial: value)
         }
     }
 }
