5af4fc4e7a8b936004558a4b35210c382756b694
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-06-01 15:56:13 +0200

    Create aliases for colors

---
+++ Engine/Aliases/simd/simd_float3+Alias.swift
@@ -0,0 +1,8 @@
+//
+//  Copyright © 2022 Mateusz Stompór. All rights reserved.
+//
+
+import simd
+
+public typealias PNColorRGB = simd_float3
+public typealias PNColorBGR = simd_float3
---
+++ Engine/Aliases/simd/simd_float4+Alias.swift
@@ -0,0 +1,8 @@
+//
+//  Copyright © 2022 Mateusz Stompór. All rights reserved.
+//
+
+import simd
+
+public typealias PNColorRGBA = simd_float4
+public typealias PNColorBGRA = simd_float4
