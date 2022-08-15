9a761cf434de4455fadfb7b8da183536782b90af
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-08-15 13:19:02 +0200

    Make default colors public

--- Engine/Extensions/simd/simd_float4+Extension.swift
+++ Engine/Extensions/simd/simd_float4+Extension.swift
@@ -11,16 +11,16 @@ extension simd_float4 {
     var zyxw: simd_float4 {
         simd_float4(z, y, x, w)
     }
-    static var defaultNormalsColor: simd_float4 {
+    public static var defaultNormalsColor: simd_float4 {
         simd_float4(1, 0.5, 0.5, 1)
     }
-    static var defaultBaseColor: simd_float4 {
+    public static var defaultBaseColor: simd_float4 {
         simd_float4(0, 1, 0, 1)
     }
-    static var defaultRoughnessColor: simd_float4 {
+    public static var defaultRoughnessColor: simd_float4 {
         simd_float4(1, 1, 1, 1)
     }
-    static var defaultMetallicColor: simd_float4 {
+    public static var defaultMetallicColor: simd_float4 {
         simd_float4(0, 0, 0, 1)
     }
 }
