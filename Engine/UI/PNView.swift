0e4143a66bb4c28029ba6e0e708d941d734514be
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-08-15 13:16:52 +0200

    Improve encapsulation

--- Engine/UI/PNView.swift
+++ Engine/UI/PNView.swift
@@ -4,28 +4,47 @@
 
 import MetalKit
 
-public class PNView: MTKView, MTKViewDelegate {
-    public var engine: PNEngine?
-    public required init(coder: NSCoder) {
+public class PNView: NSView, MTKViewDelegate {
+    public let engine: PNEngine?
+    private let metalView: MTKView
+    public var device: MTLDevice? {
+        get {
+            metalView.device
+        } set {
+            metalView.device = newValue
+        }
+    }
+    public override init(frame frameRect: NSRect) {
+        metalView = MTKView(frame: frameRect)
+        metalView.device = MTLCreateSystemDefaultDevice()
+        engine = PNIEngine.default(view: metalView)
+        super.init(frame: frameRect)
+        commonInitializer()
+    }
+    public required init?(coder: NSCoder) {
+        metalView = MTKView(coder: coder)
+        metalView.device = MTLCreateSystemDefaultDevice()
+        engine = PNIEngine.default(view: metalView)
         super.init(coder: coder)
-        initializeView()
+        commonInitializer()
     }
-    public override init(frame frameRect: CGRect, device: MTLDevice?) {
-        super.init(frame: frameRect, device: device)
-        initializeView()
-    }
-    private func initializeView() {
-        device = MTLCreateSystemDefaultDevice()
-        delegate = self
-        colorPixelFormat = .bgra8Unorm
-        framebufferOnly = false
-        autoResizeDrawable = false
-        drawableSize = CGSize(width: 1_440, height: 900)
-        preferredFramesPerSecond = 60
-        engine = PNIEngine.default(view: self,
-                                   renderingSize: drawableSize,
-                                   scene: .default,
-                                   threaded: true)
+    private func commonInitializer() {
+        addSubview(metalView)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[subview]-0-|",
+                                                      options: .directionLeadingToTrailing,
+                                                      metrics: nil,
+                                                      views: ["subview": metalView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[subview]-0-|",
+                                                      options: .directionLeadingToTrailing,
+                                                      metrics: nil,
+                                                      views: ["subview": metalView]))
+        metalView.translatesAutoresizingMaskIntoConstraints = false
+        metalView.delegate = self
+        metalView.colorPixelFormat = .bgra8Unorm
+        metalView.framebufferOnly = false
+        metalView.autoResizeDrawable = false
+        metalView.drawableSize = CGSize(width: 1_440, height: 900)
+        metalView.preferredFramesPerSecond = 60
     }
     // MTKViewDelegate
     public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
