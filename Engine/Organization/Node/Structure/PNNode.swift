d7023ffc6a4733658928355335f65866eb8c7b9b
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-08-17 15:05:22 +0200

    Add possibility to track structure changes in node

--- Engine/Organization/Node/Structure/PNNode.swift
+++ Engine/Organization/Node/Structure/PNNode.swift
@@ -2,14 +2,37 @@
 //  Copyright © 2021 Mateusz Stompór. All rights reserved.
 //
 
+import Combine
+
 public final class PNNode<T> {
-    public var data: T
-    public weak var parent: PNNode?
-    public var children: [PNNode]
+    public var dataSubject: CurrentValueSubject<T, Error>
+    public var data: T {
+        get {
+            dataSubject.value
+        } set {
+            dataSubject.send(newValue)
+        }
+    }
+    public var parentSubject: CurrentValueSubject<PNWeakRef<PNNode>, Error>
+    public weak var parent: PNNode? {
+        get {
+            parentSubject.value.reference
+        } set {
+            parentSubject.send(PNWeakRef(newValue))
+        }
+    }
+    public var childrenSubject: CurrentValueSubject<[PNNode], Error>
+    public var children: [PNNode] {
+        get {
+            childrenSubject.value
+        } set {
+            childrenSubject.send(newValue)
+        }
+    }
     public init(data: T, parent: PNNode? = nil, children: [PNNode] = []) {
-        self.data = data
-        self.parent = parent
-        self.children = children
+        parentSubject = CurrentValueSubject<PNWeakRef<PNNode>, Error>(PNWeakRef(parent))
+        dataSubject = CurrentValueSubject<T, Error>(data)
+        childrenSubject = CurrentValueSubject<[PNNode], Error>(children)
     }
     public func add(child: PNNode<T>) {
         child.parent = self
---
+++ Engine/Utilities/PNWeakRef.swift
@@ -0,0 +1,10 @@
+//
+//  Copyright © 2022 Mateusz Stompór. All rights reserved.
+//
+
+public class PNWeakRef<T: AnyObject> {
+    public weak var reference: T?
+    init(_ reference: T?) {
+        self.reference = reference
+    }
+}
