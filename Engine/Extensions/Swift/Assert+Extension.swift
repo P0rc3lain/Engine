ec1b08dca8ba391f64ec9af8ff974c214620e298
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-09-06 00:43:34 +0200

    Extend assertion methods

--- Engine/Extensions/Swift/Assert+Extension.swift
+++ Engine/Extensions/Swift/Assert+Extension.swift
@@ -2,12 +2,73 @@
 //  Copyright © 2022 Mateusz Stompór. All rights reserved.
 //
 
+import simd
+
 public func assert(value: Float,
                    expected: Float,
-                   accuracy: Float,
+                   allowedError: Float = 0,
                    message: String = "",
                    file: StaticString = #file,
                    line: UInt = #line) {
-    assert(value > expected - accuracy, message, file: file, line: line)
-    assert(value < expected + accuracy, message, file: file, line: line)
+    assertInBound(value: value,
+                  min: value - allowedError,
+                  max: value + allowedError,
+                  message: message,
+                  file: file,
+                  line: line)
+}
+
+public func assertZeroOneUnit(_ value: Float,
+                              allowedError: Float = 0,
+                              message: String = "",
+                              file: StaticString = #file,
+                              line: UInt = #line) {
+    assertInBound(value: value,
+                  min: 0,
+                  max: 1,
+                  message: message,
+                  file: file,
+                  line: line)
+}
+
+public func assertUnit(vector: simd_float3,
+                       message: String = "Must be a unit vector",
+                       allowedError: Float = 0,
+                       file: StaticString = #file,
+                       line: UInt = #line) {
+    assert(value: vector.norm,
+           expected: 1,
+           allowedError: allowedError,
+           message: message,
+           file: file,
+           line: line)
+}
+
+public func assertInBound(value: Float,
+                          min: Float,
+                          max: Float,
+                          message: String = "Value out of bounds",
+                          file: StaticString = #file,
+                          line: UInt = #line) {
+    assert(value >= min, message, file: file, line: line)
+    assert(value <= max, message, file: file, line: line)
+}
+
+public func assertValid(color: PNColor4,
+                        message: String = "Each color component must be in [0, 1] range",
+                        accuracy: Float = 0.001,
+                        file: StaticString = #file,
+                        line: UInt = #line) {
+    assertValid(color: color.xyz, message: message, file: file, line: line)
+    assertZeroOneUnit(color.w, message: message, file: file, line: line)
+}
+
+public func assertValid(color: PNColor3,
+                        message: String = "Each color component must be in [0, 1] range",
+                        accuracy: Float = 0.001,
+                        file: StaticString = #file,
+                        line: UInt = #line) {
+    assertZeroOneUnit(color.x, message: message, file: file, line: line)
+    assertZeroOneUnit(color.y, message: message, file: file, line: line)
+    assertZeroOneUnit(color.z, message: message, file: file, line: line)
 }
