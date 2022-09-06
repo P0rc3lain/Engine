2c3c1315ddc34bcbfd57be2798af04d51a14c2e1
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-09-06 18:22:27 +0200

    Extend assertions

--- Engine/Extensions/Swift/Assert+Extension.swift
+++ Engine/Extensions/Swift/Assert+Extension.swift
@@ -72,3 +72,24 @@ public func assertValid(color: PNColor3,
     assertZeroOne(color.y, message: message, file: file, line: line)
     assertZeroOne(color.z, message: message, file: file, line: line)
 }
+
+public func assertNil<T>(_ value: T?,
+                         _ message: String = "Value must be nil",
+                         file: StaticString = #file,
+                         line: UInt = #line) {
+    assert(value == nil, message, file: file, line: line)
+}
+
+public func assertSorted<T: Comparable>(_ values: [T],
+                                        _ message: String = "Collection must be sorted",
+                                        file: StaticString = #file,
+                                        line: UInt = #line) {
+    assert(values == values.sorted(), message, file: file, line: line)
+}
+
+public func assertNotEmpty<T>(_ values: [T],
+                              _ message: String = "Collection must not be empty",
+                              file: StaticString = #file,
+                              line: UInt = #line) {
+    assert(!values.isEmpty, message, file: file, line: line)
+}
