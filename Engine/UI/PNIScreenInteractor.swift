6bd6d34684999bab0c76812accbec68202c2f965
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-09-09 23:14:35 +0200

    Fix picking for orthographic camera

--- Engine/UI/PNIScreenInteractor.swift
+++ Engine/UI/PNIScreenInteractor.swift
@@ -15,8 +15,14 @@ class PNIScreenInteractor: PNScreenInteractor {
               scene: PNScene,
               point: PNPoint2D) -> [PNScenePiece] {
         let inverseProjection = cameraNode.camera.projectionMatrixInverse
+        var far = [0, 0, 1, 1] * inverseProjection
+        var near = [0, 0, 0, 1] * inverseProjection
+        far /= far.w
+        near /= near.w
+        let isPerspective = far.z == near.z
         let direction = inverseProjection * simd_float4(point.x, point.y, 1, 1)
-        let rayEye = PNRay(origin: .zero, direction: direction.xyz.normalized)
+        let origin = isPerspective ? .zero : simd_float3(direction.x, direction.y, 0)
+        let rayEye = PNRay(origin: origin, direction: direction.xyz.normalized)
         let rayWorld = cameraNode.worldTransform.value * rayEye
         return nodeInteractor.deepSearch(from: scene.rootNode) { node in
             guard let value = node.data.worldBoundingBox.value else {
