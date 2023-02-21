c7859af119575a43b60222346d821d4189f89163
Mateusz Stompór <mateusz.stompor@gmail.com>
2023-02-21 17:39:01 +0100

    Describe aliases

--- Engine/Core/Scene/Bounds/BoundingBox/PNBoundingBox.swift
+++ Engine/Core/Scene/Bounds/BoundingBox/PNBoundingBox.swift
@@ -4,6 +4,8 @@
 
 import simd
 
+/// Specifies all extreme points of a given abstract.
+/// In contrary to ``PNBound`` definitions are explicit.
 public struct PNBoundingBox: Equatable {
     let cornersLower: simd_float4x4
     let cornersUpper: simd_float4x4
