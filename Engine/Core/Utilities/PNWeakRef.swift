a322739674ba79f7dafef519011df793285e883d
Mateusz Stompór <mateusz.stompor@gmail.com>
2023-02-21 17:37:17 +0100

    Describe PNWeakRef

--- Engine/Core/Utilities/PNWeakRef.swift
+++ Engine/Core/Utilities/PNWeakRef.swift
@@ -2,6 +2,7 @@
 //  Copyright © 2022 Mateusz Stompór. All rights reserved.
 //
 
+/// Used to allow possibility of keeping weak references to objects in a collection
 public class PNWeakRef<T: AnyObject> {
     public weak var reference: T?
     public init(_ reference: T?) {
