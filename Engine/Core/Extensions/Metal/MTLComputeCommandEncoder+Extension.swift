e55670d173eedee3ab7e49fbd98efc51f081c2de
Mateusz Stompór <mstompor@motionvfx.com>
2023-09-03 16:16:38 +0200

    Fix pointer warnings

--- Engine/Core/Extensions/Metal/MTLComputeCommandEncoder+Extension.swift
+++ Engine/Core/Extensions/Metal/MTLComputeCommandEncoder+Extension.swift
@@ -5,20 +5,26 @@
 import Metal
 
 extension MTLComputeCommandEncoder {
-    func setTexture<T: RawRepresentable>(_ provider: PNTextureProvider, index: T) where T.RawValue == UInt32 {
+    func setTexture<T: RawRepresentable>(_ provider: PNTextureProvider,
+                                         index: T) where T.RawValue == UInt32 {
         setTexture(provider.texture, index: index.int)
     }
-    func setBuffer<T: RawRepresentable, Z>(_ dynamicBuffer: PNAnyDynamicBuffer<Z>?, offset: Int, index: T) where T.RawValue == UInt32 {
+    func setBuffer<T: RawRepresentable, Z>(_ dynamicBuffer: PNAnyDynamicBuffer<Z>?,
+                                           offset: Int,
+                                           index: T) where T.RawValue == UInt32 {
         setBuffer(dynamicBuffer?.buffer, offset: 0, index: index.int)
     }
-    func setBuffer<T: RawRepresentable, Z>(_ dynamicBuffer: PNAnyDynamicBuffer<Z>?, index: T) where T.RawValue == UInt32 {
+    func setBuffer<T: RawRepresentable, Z>(_ dynamicBuffer: PNAnyDynamicBuffer<Z>?,
+                                           index: T) where T.RawValue == UInt32 {
         setBuffer(dynamicBuffer, offset: 0, index: index)
     }
-    func setBuffer<T: RawRepresentable, Z>(_ staticBuffer: PNAnyStaticBuffer<Z>?, index: T) where T.RawValue == UInt32 {
+    func setBuffer<T: RawRepresentable, Z>(_ staticBuffer: PNAnyStaticBuffer<Z>?,
+                                           index: T) where T.RawValue == UInt32 {
         setBuffer(staticBuffer?.buffer, offset: 0, index: index.int)
     }
     func setBytes<P, T: RawRepresentable>(_ value: P, index: T) where T.RawValue == UInt32 {
-        var tmp = value
-        setBytes(&tmp, length: MemoryLayout<P>.size, index: index.int)
+        withUnsafePointer(to: value) { ptr in
+            setBytes(ptr, length: MemoryLayout<P>.size, index: index.int)
+        }
     }
 }
