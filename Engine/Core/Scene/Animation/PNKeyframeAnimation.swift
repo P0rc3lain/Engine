c3d2df2424a5cc2e565cc3b9fbe7b89bea942f25
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-04-05 13:46:19 +0200

    Animation docs

--- Engine/Core/Scene/Animation/PNKeyframeAnimation.swift
+++ Engine/Core/Scene/Animation/PNKeyframeAnimation.swift
@@ -2,15 +2,31 @@
 //  Copyright © 2022 Mateusz Stompór. All rights reserved.
 //
 
-/// Action in a digital animation sequence.
-/// Define points that later will be interpolated to estimate inbetween values.
+/// This struct defines keyframes and their corresponding time points
+/// for interpolation in an animation sequence. The generic type `T`
+/// represents the type of value being animated (e.g., position, color, etc.).
 public struct PNKeyframeAnimation<T> {
+    /// An array of keyframes that define the main points in the animation sequence.
+    /// Each keyframe represents a specific state of type `T` at a given time point.
     public var keyFrames: [T]
+    /// An array of time points corresponding to each keyframe.
+    /// Must be sorted in ascending order and have the same count as `keyFrames`.
     public let times: [PNTimePoint]
-    public let maximumTime: PNTimeInterval
+    /// The maximum time for the animation sequence.
+    /// Must be greater than or equal to the last time point.
+    public let maximumTime: PNTimePoint
+    /// Initializes a keyframe animation with keyframes, times, and maximum time.
+    /// - Parameters:
+    ///   - keyFrames: Array of animation keyframes of type `T`
+    ///   - times: Array of time points for each keyframe
+    ///   - maximumTime: The maximum time point for the animation
+    /// - Precondition: `times` array must not be empty
+    /// - Precondition: `times` array must be sorted in ascending order
+    /// - Precondition: `times.count` must equal `keyFrames.count`
+    /// - Precondition: `maximumTime` must be greater than or equal to the last time point
     public init(keyFrames: [T],
                 times: [PNTimePoint],
-                maximumTime: PNTimeInterval) {
+                maximumTime: PNTimePoint) {
         assertEqual(times.count, keyFrames.count)
         assertSorted(times)
         assertNotEmpty(times)
@@ -19,6 +35,9 @@ public struct PNKeyframeAnimation<T> {
         self.times = times
         self.maximumTime = maximumTime
     }
+    /// Creates a static animation with a single keyframe.
+    /// - Parameter value: The value for the static keyframe
+    /// - Returns: A PNKeyframeAnimation with a single keyframe at time 0 and maximum time 1
     static public func `static`(from value: T) -> PNKeyframeAnimation<T> {
         PNKeyframeAnimation<T>(keyFrames: [value],
                                times: [0],
