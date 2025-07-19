19209738c322d0611237ec72d54342a6b18075e9
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-07-19 10:01:47 +0200

    Add safe bundle retrieval

--- Engine/Core/Extensions/Foundation/Bundle+Extension.swift
+++ Engine/Core/Extensions/Foundation/Bundle+Extension.swift
@@ -5,8 +5,17 @@
 import Foundation
 
 extension Bundle {
+    /// An empty class used solely to anchor the bundle lookup to the current module.
     fileprivate class Unnamed { }
+    /// Returns the bundle associated with the current module, using the Unnamed marker class.
     static var current: Bundle {
         Bundle(for: Unnamed.self)
+    }
+    /// Retrieves the bundle identifier, terminating with a descriptive error if unavailable.
+    var bundleIdentifierSafe: String {
+        guard let bundleIdentifier = bundleIdentifier else {
+            fatalError("Could not retrieve bundle identifier")
+        }
+        return bundleIdentifier
     }
 }
