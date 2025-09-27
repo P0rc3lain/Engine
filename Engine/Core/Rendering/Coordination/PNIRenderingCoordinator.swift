627822744ff3e9ecde9e4eb2dc386a7b1e0a78d5
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-09-28 00:04:36 +0200

    Move whole rendering logic to one place

--- Engine/Core/Rendering/Coordination/PNIRenderingCoordinator.swift
+++ Engine/Core/Rendering/Coordination/PNIRenderingCoordinator.swift
@@ -9,42 +9,25 @@ import os.signpost
 import simd
 
 struct PNIRenderingCoordinator: PNRenderingCoordinator {
-    private let view: MTKView
     private let commandQueue: MTLCommandQueue
     private var pipeline: PNPipeline
-    private let imageConverter: MPSImageConversion
     init?(view metalView: MTKView, renderingSize: CGSize) {
         guard let device = metalView.device,
               let commandQueue = device.makeCommandQueue(),
               let pipeline = PNPipeline(device: device,
-                                        renderingSize: renderingSize) else {
+                                        renderingSize: renderingSize,
+                                        view: metalView) else {
             return nil
         }
-        self.imageConverter = MPSImageConversion(device: device)
-        self.view = metalView
         self.pipeline = pipeline
         self.commandQueue = commandQueue
     }
     mutating func draw(frameSupply: PNFrameSupply) {
-        guard frameSupply.scene.activeCameraIdx != .nil,
-              let drawable = view.currentDrawable,
-              let outputTexture = view.currentRenderPassDescriptor?.colorAttachments[0].texture,
-              let sourceTexture = pipeline.io.output.color[0].texture else {
+        guard frameSupply.scene.activeCameraIdx != .nil else {
             return
         }
         let encodingInterval = psignposter.beginInterval("Frame encoding")
         pipeline.draw(commandQueue: commandQueue, supply: frameSupply)
-        guard let commandBuffer = commandQueue.makeCommandBuffer() else {
-            fatalError("Could create command buffer for frame copying")
-        }
-        commandBuffer.pushDebugGroup("Copy Pass")
-        imageConverter.encode(commandBuffer: commandBuffer,
-                              sourceTexture: sourceTexture,
-                              destinationTexture: outputTexture)
-        commandBuffer.popDebugGroup()
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
         psignposter.endInterval("Frame encoding", encodingInterval)
-        commandBuffer.waitUntilCompleted()
     }
 }
